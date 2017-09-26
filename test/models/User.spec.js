ed363839feac08dc6baf9733b0bee42ce1d24fe9
Derek Miranda <derek.miranda.1992@gmail.com>
2017-09-26 07:56:23 -0700

    User model test

---
+++ test/models/User.spec.js
@@ -0,0 +1,24 @@
+import test from 'ava';
+
+import db from '../../models';
+import { dbSetup } from '../../utils/tests';
+
+dbSetup(test);
+
+test.serial('should reject passwords shorter than 8 chars', async t => {
+  const { User } = db;
+  await t.throws(User.create({
+    username: 'derek',
+    email: 'derek@miranda.com',
+    passHash: 'secure',
+  }))
+})
+
+test.serial('should reject passwords longer than 16 chars', async t => {
+  const { User } = db;
+  await t.throws(User.create({
+    username: 'derek',
+    email: 'derek@miranda.com',
+    passHash: 'securesecuresecuresecure',
+  }))
+})
