27e314fd1cd062517dda6ec9c59e83d3fd08478a
Derek Miranda <derek.miranda.1992@gmail.com>
2017-09-27 09:11:51 -0700

    Renamed: viewing -> viewings

---
+++ server/routers/movieViewingsRouter.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const { makeJSONResponseMiddleware } = require('./utils');
+const movieViewingsController = require('../controllers/movieViewingsController');
+
+const movieViewingsRouter = express.Router();
+
+movieViewingsRouter.get('/', makeJSONResponseMiddleware(movieViewingsController.getMovieViewings));
+
+movieViewingsRouter.post('/', (req, res, next) => {
+  const { movieViewing } = req.body;
+  const middleware = makeJSONResponseMiddleware(movieViewingsController.addMovieViewing, movieViewing);
+  return middleware(req, res, next);
+});
+
+movieViewingsRouter.patch('/', (req, res, next) => {
+  const { movieViewings } = req.body;
+  const middleware = makeJSONResponseMiddleware(movieViewingsController.updateMovieViewings, movieViewings);
+  return middleware(req, res, next);
+});
+
+movieViewingsRouter.delete('/:id', (req, res, next) => {
+  const { id } = req.params;
+  const middleware = makeJSONResponseMiddleware(movieViewingsController.removeMovieViewing, id);
+  return middleware(req, res, next);
+});
+
+module.exports = movieViewingsRouter;
