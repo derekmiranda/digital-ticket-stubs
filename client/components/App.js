7ab05a84404bd7eac3d98e2119a6fadf7df58f68
Derek Miranda <derek.miranda.1992@gmail.com>
2017-10-02 08:21:29 -0700

    Renders dummy component

--- client/components/App.js
+++ client/components/App.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 import createViewingsContainer from 'containers/createViewingsContainer';
 
+const SomeViewingsContainer = createViewingsContainer(
+  () => <p>Hey.</p>
+)
+
 const App = () => (
-  <p>Hey.</p>
+  <SomeViewingsContainer />
 )
 
 export default App;
