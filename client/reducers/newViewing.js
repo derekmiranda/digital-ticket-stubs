f913b38810b99c692f50df36ab22d076669d8527
Derek Miranda <derek.miranda.1992@gmail.com>
2017-10-06 08:17:00 -0700

    NewViewing depends on viewing reducer

--- client/reducers/newViewing.js
+++ client/reducers/newViewing.js
@@ -1,12 +1,14 @@
 import { EDIT_NEW_VIEWING } from 'actions/types';
+import { editViewing } from 'actions/creators';
+import { viewing } from './viewings';
 
-const newViewing = (state = {}, action = {}) => {
+const newViewing = (state = viewing(), action = {}) => {
   switch (action.type) {
     case EDIT_NEW_VIEWING:
-      return {
-        ...state,
-        [action.key]: action.val,
-      }
+      return viewing(state, editViewing({
+        key: action.key,
+        val: action.val,
+      }))
     default:
       return state;
   }
