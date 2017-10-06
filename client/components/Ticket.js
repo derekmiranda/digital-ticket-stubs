4a393b1b0db909bee8bd3cee9aef7558d0ba9a12
Derek Miranda <derek.miranda.1992@gmail.com>
2017-10-06 09:18:47 -0700

    Filled out Ticket components

--- client/components/Ticket.js
+++ client/components/Ticket.js
@@ -1,1 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+const Ticket = ({ viewing }) => {
+	return (
+		<div />
+	)
+}
+
+Ticket.propTypes = {
+	
+}
+
+export default Ticket;
--- client/containers/ViewingTicket.js
+++ client/containers/ViewingTicket.js
@@ -1,1 +1,9 @@
+import { connect } from 'react-redux';
+
 import Ticket from 'components/Ticket';
+
+const mapStateToProps = state => state;
+
+const ViewingTicket = connect(mapStateToProps)(Ticket);
+
+export default ViewingTicket;
