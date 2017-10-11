8a879a5d14e4c44941faa8afc0166e67d1a68dd6
Derek Miranda <derek.miranda.1992@gmail.com>
2017-10-11 09:58:01 -0700

    Creates Viewing Ticket for every viewing

--- client/components/GridFormat/index.js
+++ client/components/GridFormat/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import ViewingTicket from 'containers/ViewingTicket';
 import { viewingSchema } from 'schemas';
 
 const GridFormat = ({ viewings }) => {
-	return <div />;
+	const tickets = viewings.map(
+		viewing => <ViewingTicket viewing={viewing}/>
+	)
+
+	return (
+		<div>
+			{tickets}
+		</div>
+	)
 }
 
 GridFormat.propTypes = {
