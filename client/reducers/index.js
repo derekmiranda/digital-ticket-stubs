c25a9ba59347657222db6e88599c0413f369f6ed
Derek Miranda <derek.miranda.1992@gmail.com>
2017-10-11 09:58:33 -0700

    Added search results

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -3,11 +3,13 @@ import { combineReducers } from 'redux';
 import viewings from './viewings';
 import newViewing from './newViewing';
 import modalOpen from './modalOpen';
+import searchResults from './searchResults';
 
 const rootReducer = combineReducers({
   viewings,
   newViewing,
   modalOpen,
+  searchResults,
 })
 
 export default rootReducer;
