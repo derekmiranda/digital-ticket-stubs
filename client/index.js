d9c86690023f60de2290c356248b88d0fa549090
Derek Miranda <derek.miranda.1992@gmail.com>
2017-10-15 12:34:53 -0700

    Added logger

--- client/index.js
+++ client/index.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import ReactDom from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
 
 import App from 'components/App';
 import reducer from 'reducers';
 
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(logger));
 
 ReactDom.render(
   <Provider store={store}>
