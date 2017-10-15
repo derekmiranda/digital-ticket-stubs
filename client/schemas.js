bd2ba3e929421e1e7abaebe6b56493191d90e1f4
Derek Miranda <derek.miranda.1992@gmail.com>
2017-10-15 11:32:05 -0700

    Unrequired title

--- client/schemas.js
+++ client/schemas.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 
 export const viewingSchema = PropTypes.shape({
-	title: PropTypes.string.isRequired,
+	title: PropTypes.string,
 	venue: PropTypes.string,
 	watchtime: PropTypes.string,
 })
