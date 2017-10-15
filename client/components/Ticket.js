d9ecd02dc7bc9c2e555587a20855408f46af6255
Derek Miranda <derek.miranda.1992@gmail.com>
2017-10-15 06:33:57 -0700

    Values to inputs

--- client/components/Ticket.js
+++ client/components/Ticket.js
@@ -2,15 +2,33 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { viewingSchema } from 'schemas';
+import getReadableFieldName from 'utils/getReadableFieldName';
 
-const Ticket = ({ viewing }) => {
+const Ticket = ({ viewing = {} }) => {
+	const inputList = viewingToInputs(viewing);
 	return (
-		<div>hi</div>
+		<div>
+			{inputList}
+		</div>
 	)
 }
 
 Ticket.propTypes = {
 	viewing: viewingSchema,
+}
+
+function viewingToInputs(viewing) {
+	const inputs = Object.keys(viewing).map((field, i) => {
+		const val = viewing[field];
+		const readableField = getReadableFieldName(field);
+		return (
+			<input type="text"
+				placeholder={readableField}
+				value={val}
+			/>
+		)
+	})
+	return inputs;
 }
 
 export default Ticket;
--- test/client/components/Ticket.spec.js
+++ test/client/components/Ticket.spec.js
@@ -1,3 +1,39 @@
 import test from 'ava';
+import React from 'react';
+import { shallow } from 'enzyme';
 
-test.todo('ghgjgh')
+import Ticket from 'components/Ticket';
+import { testRendering, initEnzyme } from 'utils/tests';
+
+initEnzyme();
+
+function initCtx() {
+  const ctx = {};
+  
+  ctx.sample = {
+    id: 1,
+    title: 'B- Movie',
+    venue: 'The Honeycomb',
+    watchtime: '2017-10-15T11:23:54.659Z',
+  }
+
+  ctx.wrapper = shallow(<Ticket viewing={ctx.sample} />);
+
+  return ctx;
+}
+
+testRendering(test, <Ticket />);
+
+const inputTestMacro = (t, field) => {
+  const { sample, wrapper } = initCtx();
+  console.log(wrapper.html());
+  const inputWrapper = wrapper.findWhere(
+    node => node.type() === 'input' && node.prop('value') === sample[field]
+  )
+  t.is(inputWrapper.length, 1);
+}
+
+inputTestMacro.title = (field) => `Renders input for ${field}`;
+
+test('title', inputTestMacro, 'title');
+test('venue', inputTestMacro, 'venue');
