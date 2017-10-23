52194db4abc11543e4341fa367c8e11cf1a1c505
Derek Miranda <derek.miranda.1992@gmail.com>
2017-10-23 08:58:04 -0700

    Gave newViewings ids

--- client/reducers/newViewings.js
+++ client/reducers/newViewings.js
@@ -1,13 +1,17 @@
 import { EDIT_NEW_VIEWING, ADD_NEW_VIEWING } from 'actions/types';
 import { editViewing, addViewing } from 'actions/creators';
-import viewings from './viewings';
+import viewings, { viewing } from './viewings';
 
+let count = 1;
 const newViewings = (state = viewings(), action = {}) => {
   switch (action.type) {
     case EDIT_NEW_VIEWING:
       return viewings(state, editViewing(action));
     case ADD_NEW_VIEWING:
-      return viewings(state, addViewing({}));
+      return viewings(state, addViewing({
+        ...viewing(),
+        id: count++,
+      }));
     default:
       return state;
   }
