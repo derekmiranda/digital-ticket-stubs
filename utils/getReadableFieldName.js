31e93ad6ee7a9152ebd4912e756a2c0fd105ad87
Derek Miranda <derek.miranda.1992@gmail.com>
2017-10-26 08:39:53 -0700

    Use object for special cases

--- utils/getReadableFieldName.js
+++ utils/getReadableFieldName.js
@@ -1,8 +1,9 @@
 const capitalize = str => str[0].toUpperCase() + str.slice(1);
 
 export default function getReadableFieldName(field) {
-  if (field === 'watchtime') {
-    return 'Watch Time';
+  const specialCases = {
+    watchtime: 'Watch Time',
   }
-  return capitalize(field);
+
+  return specialCases[field] || capitalize(field);
 }
