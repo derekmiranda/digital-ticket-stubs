3eede2287551786e6925ba6a92db987429f5dab3
Derek Miranda <derek.miranda.1992@gmail.com>
2017-11-06 08:43:09 -0800

    Using thunks

--- client/index.js
+++ client/index.js
@@ -3,11 +3,12 @@ import ReactDom from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 
 import App from 'components/App';
 import reducer from 'reducers';
 
-const store = createStore(reducer, applyMiddleware(logger));
+const store = createStore(reducer, applyMiddleware(thunk, logger));
 
 ReactDom.render(
   <Provider store={store}>
