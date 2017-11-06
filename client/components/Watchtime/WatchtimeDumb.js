06a80a76a5469c684d80faace53281da19e2ecc8
Derek Miranda <derek.miranda.1992@gmail.com>
2017-11-05 21:53:15 -0800

    Starting on watchtime editing

--- client/components/Watchtime/WatchtimeDumb.js
+++ client/components/Watchtime/WatchtimeDumb.js
@@ -1,32 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import getReadableFieldName from 'utils/getReadableFieldName';
+import getReadableFieldName from 'client/utils/getReadableFieldName';
 import {
 	createAscendingOptionsRange,
 	createDescendingOptionsRange
 } from './optionsRangeFns';
 
-const WatchtimeDumb = ({ month, day, year }) => {
+const WatchtimeDumb = ({ month, day, year, onTimeUnitChange }) => {
 	const monthSelect = createTimeSelect({
 		name: 'month',
-		options: createAscendingOptionsRange(1, 12, 'Month'),
 		value: month,
-	})
+		onChange: onTimeUnitChange('month'),
+	}, createAscendingOptionsRange(1, 12, 'Month'));
 
 	const daySelect = createTimeSelect({
 		name: 'day',
-		options: createAscendingOptionsRange(1, 30, 'Day'),
 		value: day,
-  })
+		onChange: onTimeUnitChange('day'),
+  }, createAscendingOptionsRange(1, 30, 'Day'));
   
   const currYear = new Date().getFullYear();
 	const yearOptions = createDescendingOptionsRange(currYear, 1920, 'Year');
 	const yearSelect = createTimeSelect({
 		name: 'year',
-		options: yearOptions,
 		value: year,
-	})
+		onChange: onTimeUnitChange('year'),
+	}, yearOptions)
 
 	return (
 		<div className='watchtime'>
@@ -43,14 +43,10 @@ WatchtimeDumb.propTypes = {
 	year: PropTypes.number,
 }
 
-function createTimeSelect ({ name, options, value }) {
+function createTimeSelect (props, options) {
 	const capitalize = str => str[0].toUpperCase() + str.slice(1);
 	return (
-		<select
-			name={name}
-			className={name}
-			value={value}
-		>
+		<select {...props}>
 			{options}
 		</select>
 	)
