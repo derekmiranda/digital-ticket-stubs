4d7b576e4b36f09cc4967d088040adc30db97866
Derek Miranda <derek.miranda.1992@gmail.com>
2017-11-07 10:17:52 -0800

    Added .env loading

--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,32 @@ const webpack = require('webpack');
 
 const WDS_PORT = 8000;
 
+// load environment vars
+const loadedEnv = require('dotenv').load(
+  { path: path.join(__dirname, 'config', `.${process.env.NODE_ENV}.env`) }
+);
+
+const stringifyObjVals = (obj) => {
+  return Object.keys(obj).reduce((accum, key) => {
+    accum[key] = JSON.stringify(obj[key]);
+    return accum;
+  }, {});
+}
+
+const processEnv = Object.assign({
+  NODE_ENV: process.env.NODE_ENV,
+}, loadedEnv.parsed);
+
+const stringifiedProcessEnv = stringifyObjVals(processEnv);
+
+// plugins
 const plugins = [];
 
 if (process.env.NODE_ENV === 'development') {
   plugins.push(
     new webpack.HotModuleReplacementPlugin(),
     new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
-      }
+      'process.env': stringifiedProcessEnv,
     })
   )
 }
