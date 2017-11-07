3da950b38f3046b77b3db6f12dd38f66f1f4bf96
Derek Miranda <derek.miranda.1992@gmail.com>
2017-11-07 11:48:43 -0800

    Starting requests code

--- client/sagas/index.js
+++ client/sagas/index.js
@@ -1,3 +1,58 @@
 import { call, put, takeLatest, takeEvery } from 'redux-saga/effects';
 
-import { fetchViewings, saveNewViewing, removeViewing } from '../services/viewingsApi';
+import { fetchViewings, saveNewViewing, removeViewing } from 'services/moviesApi';
+
+/* Adjust */
+import { FETCH_STARTED, CREATE_NEW_MOVIE, REQUEST_DELETE_MOVIE } from '../actions/types';
+import {
+  fetchSucceeded, 
+  addViewing, 
+  requestDeleteViewing, 
+  deleteViewing,
+  clearNewViewing,
+} from '../actions/creators';
+/*  */
+
+export function* fetchData(action) {
+  try {
+    const movies = yield call(fetchViewings);
+    yield put(fetchSucceeded(movies));
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+function* watchFetchData() {
+  yield takeLatest(FETCH_STARTED, fetchData);
+}
+
+export function* postViewing({ movie }) {
+  try {
+    const newViewing = yield call(saveNewViewing, movie);
+    yield put(addViewing(newViewing));
+    yield put(clearNewViewing())
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+function* watchSaveNewViewing() {
+  yield takeEvery(CREATE_NEW_MOVIE, postViewing);
+}
+
+export function* clearViewing({ movie, movieIdx }) {
+  try {
+    yield call(removeViewing, movie);
+    yield put(deleteViewing(movieIdx));
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+function* watchRequestDeleteViewing() {
+  yield takeEvery(REQUEST_DELETE_MOVIE, clearViewing);
+}
+
+export default function* rootSaga() {
+  yield [watchFetchData(), watchSaveNewViewing(), watchRequestDeleteViewing()];
+}
---
+++ client/services/viewingsApi.js
@@ -0,0 +1,41 @@
+import { processViewingsFromDb, processViewingsFromClient } from './processing';
+
+export const fetchViewings = () => {
+  return fetch(process.env.VIEWINGS_API_URL)
+    .then(res => res.json())
+    .then(processViewingsFromDb)
+}
+
+export const updateViewings = (viewings) => {
+  const processedViewings = processViewingsFromDb(viewings);
+  return fetch(process.env.VIEWINGS_API_URL, {
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    method: 'PATCH',
+    body: processedViewings,
+  })
+    .then(res => res.json())
+}
+
+export const saveNewViewing = (viewing) => {
+  const body = JSON.stringify({ viewing });
+
+  return fetch(process.env.VIEWINGS_API_URL, {
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    method: 'POST',
+    body,
+  })
+    .then(res => res.json())
+}
+
+export const removeViewing = (viewing) => {
+  return fetch(`${process.env.VIEWINGS_API_URL}/${viewing.id}`, {
+    method: 'DELETE',
+  })
+    .then(res => res.json())
+}
