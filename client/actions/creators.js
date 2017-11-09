5c96bb6d39aae78b3ee4373f6edc444f18e9e524
Derek Miranda <derek.miranda.1992@gmail.com>
2017-11-09 07:16:30 -0800

    Removed creators that are taken care of by redux form

--- client/actions/creators.js
+++ client/actions/creators.js
@@ -6,59 +6,6 @@ export const createRequestAction = (reqAction) => ({
   type: `REQUEST_${reqAction.type}`,
 })
 
-// viewing
-export const editViewing = ({ id, key, val }) => ({
-  type: types.EDIT_VIEWING,
-  id,
-  key,
-  val,
-})
-
-export const removeViewing = (id) => ({
-  type: types.REMOVE_VIEWING,
-  id,
-})
-
-export const addViewing = (viewing) => ({
-  type: types.ADD_VIEWING,
-  viewing,
-})
-
-export const fetchedViewings = (viewings) => ({
-  type: types.FETCHED_VIEWINGS,
-  viewings,
-})
-
-// new viewing
-export const editNewViewing = (...args) => ({
-  ...editViewing(...args),
-  type: types.EDIT_NEW_VIEWING,
-})
-
-export const addNewViewing = createSimpleActionCreator(types.ADD_NEW_VIEWING);
-
-export const saveNewViewing = (newViewing) => ({
-  type: types.SAVE_NEW_VIEWING,
-  newViewing,
-})
-
-// watchtime
-export const editWatchtime = ({
-  id,
-  timeUnit,
-  val,
-}) => ({
-  type: types.EDIT_WATCHTIME,
-  id,
-  timeUnit,
-  val,
-})
-
-export const editNewWatchtime = (...args) => ({
-  ...editWatchtime(...args),
-  type: types.EDIT_NEW_WATCHTIME,
-})
-
 // modal
 export const focusViewing = (id) => ({
   type: types.FOCUS_VIEWING,
