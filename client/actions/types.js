a00a3f61cf2d6f1b540f4fe169896def6466d819
Derek Miranda <derek.miranda.1992@gmail.com>
2017-11-12 09:26:21 -0800

    Removed extraneous types

--- client/actions/types.js
+++ client/actions/types.js
@@ -1,18 +1,3 @@
-// saved viewings
-export const EDIT_VIEWING = 'EDIT_VIEWING';
-export const REMOVE_VIEWING = 'REMOVE_VIEWING';
-export const ADD_VIEWING = 'ADD_VIEWING';
-export const FETCHED_VIEWINGS = 'FETCHED_VIEWINGS';
-
-// new viewing
-export const EDIT_NEW_VIEWING = 'EDIT_NEW_VIEWING';
-export const SAVE_NEW_VIEWING = 'SAVE_NEW_VIEWING';
-export const ADD_NEW_VIEWING = 'ADD_NEW_VIEWING';
-
-// watchtime
-export const EDIT_WATCHTIME = 'EDIT_WATCHTIME';
-export const EDIT_NEW_WATCHTIME = 'EDIT_NEW_WATCHTIME';
-
 // modal
 export const FOCUS_VIEWING = 'FOCUS_VIEWING';
 
