fc6b3caaba60bb9542dcd7316a9d97afc0cfe47d
Derek Miranda <derek.miranda.1992@gmail.com>
2017-11-12 20:07:50 -0800

    Logic for preventing ticket submission if any errors

--- client/containers/TicketContainer.js
+++ client/containers/TicketContainer.js
@@ -1,23 +1,52 @@
 import { connect } from 'react-redux';
-import { getFormValues, change } from 'redux-form';
+import {
+  getFormValues,
+  change,
+  getFormSyncErrors,
+  getFormAsyncErrors,
+  getFormSubmitErrors
+} from 'redux-form';
 import PropTypes from 'prop-types';
-import { curry } from 'lodash-es';
 
 import Ticket from 'components/Ticket';
 import { ticketsFormName as formName } from 'client/constants';
 import debug from 'client/utils/debug';
 
 const formSelector = getFormValues(formName);
+const syncErrorsSelector = getFormSyncErrors(formName);
+const asyncErrorsSelector = getFormAsyncErrors(formName);
+const submitErrorsSelector = getFormSubmitErrors(formName);
 
 const mapStateToProps = state => state;
+
+const numErrors = errObj => Object.keys(errObj).length;
+const getErrorsForViewing= (state, idx, errorSelectors) => {
+  if (!errorSelectors.length) return null;
+  const currErrorSelector = errorSelectors[0];
+  const errors = currErrorSelector(state);
+  const errorsForViewing = errors && errors.viewings && errors.viewings[idx];
+  return errorsForViewing && numErrors(errorsForViewing)
+    ? errorsForViewing
+    : getErrorsForViewing(state, idx, errorSelectors.slice(1));
+}
+
 const mapDispatchToProps = (dispatch, { idx, name }) => {
   const createSubmitHandlerWithState = (state) => () => {
     const formState = formSelector(state);
-    debug('form state', formState);
-    const { viewings } = formState;
-    const viewing = viewings[idx];
-    debug(viewing);
-    dispatch(change(formName, `${name}.id`, 1000))
+    const errors = getErrorsForViewing(state, idx, [
+      syncErrorsSelector,
+      asyncErrorsSelector,
+      submitErrorsSelector,
+    ]);
+
+    if (errors) {
+      debug('Errors:', errors);
+    } else {
+      const { viewings } = formState;
+      const viewing = viewings[idx];
+      debug('Viewing:', viewing);
+      dispatch(change(formName, `${name}.id`, 1000))
+    }
   }
   return { createSubmitHandlerWithState };
 }
