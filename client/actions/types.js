67d9cc017d8a4ad1f1561bc945860f158ee82654
Derek Miranda <derek.miranda.1992@gmail.com>
2017-11-12 20:07:12 -0800

    Ticket submit action types

--- client/actions/types.js
+++ client/actions/types.js
@@ -4,7 +4,10 @@ export const FOCUS_VIEWING = 'FOCUS_VIEWING';
 // search results
 export const FETCHED_SEARCH_RESULTS = 'FETCHED_SEARCH_RESULTS';
 
-// error handling
+// requests
+export const TICKET_SUBMIT = 'TICKET_SUBMIT';
+export const START_TICKET_SUBMIT = 'START_TICKET_SUBMIT';
+export const TICKET_SUBMIT_SUCCEEDED = 'TICKET_SUBMIT_SUCCEEDED';
 export const REQUEST_ERROR = 'REQUEST_ERROR';
 
 // dynamic types for actions requiring server requests
