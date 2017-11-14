b44f5501ec6e04679fccf29bce908177b88a2694
Derek Miranda <derek.miranda.1992@gmail.com>
2017-11-14 08:33:22 -0800

    Base reducer plugin implementation

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,10 +1,18 @@
 import { combineReducers } from 'redux';
-
 import { reducer as formReducer } from 'redux-form';
+
 import modalOpen from './modalOpen';
+import { ticketsFormName } from 'client/constants';
 
 const rootReducer = combineReducers({
-  form: formReducer,
+  form: formReducer.plugin({
+    [ticketsFormName]: (state, action = {}) => {
+      switch (action.type) {
+        default:
+          return state;
+      }
+    }
+  }),
   modalOpen,
 })
 
