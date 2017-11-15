6b95663d6f3074a82bec77e58c10fbf01cfa8200
Derek Miranda <derek.miranda.1992@gmail.com>
2017-11-15 06:39:40 -0800

    Added other ticket submit types

--- client/actions/types.js
+++ client/actions/types.js
@@ -5,9 +5,10 @@ export const FOCUS_VIEWING = 'FOCUS_VIEWING';
 export const FETCHED_SEARCH_RESULTS = 'FETCHED_SEARCH_RESULTS';
 
 // requests
-export const TICKET_SUBMIT = 'TICKET_SUBMIT';
 export const START_TICKET_SUBMIT = 'START_TICKET_SUBMIT';
+export const STOP_TICKET_SUBMIT = 'STOP_TICKET_SUBMIT';
 export const TICKET_SUBMIT_SUCCEEDED = 'TICKET_SUBMIT_SUCCEEDED';
+export const TICKET_SUBMIT_FAILED = 'TICKET_SUBMIT_FAILED';
 export const REQUEST_ERROR = 'REQUEST_ERROR';
 
 // dynamic types for actions requiring server requests
