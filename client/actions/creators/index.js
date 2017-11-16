38836a86310bdd70a744c1e1bf20b8308b54ea5a
Derek Miranda <derek.miranda.1992@gmail.com>
2017-11-16 08:25:56 -0800

    Added thunk

--- client/actions/creators/index.js
+++ client/actions/creators/index.js
@@ -1,4 +1,4 @@
-import * as types from './types';
+import * as types from '../types';
 
 export const createSimpleActionCreator = type => () => ({ type });
 export const createRequestAction = (reqAction) => ({
--- client/index.js
+++ client/index.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import ReactDom from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 
 import App from 'components/App';
 import reducer from 'reducers';
 
-const middleware = [];
+const middleware = [
+  thunk,
+];
+
 if (process.env.NODE_ENV === 'development') {
   middleware.push(logger);
 }
