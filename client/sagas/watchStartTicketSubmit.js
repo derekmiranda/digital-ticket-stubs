511dda0f63ccc8668e50b9c0065f30940c66ac07
Derek Miranda <derek.miranda.1992@gmail.com>
2017-11-16 15:34:31 -0800

    Can create new ticket/viewing on db, sets client-side instance's id

--- client/sagas/watchStartTicketSubmit.js
+++ client/sagas/watchStartTicketSubmit.js
@@ -22,36 +22,33 @@ import {
 } from '../actions/creators';
 import { ticketsFormName } from 'client/constants';
 
-function* saveViewing({
-  viewing,
-  index,
-  viewingName,
-}) {
-  try {
-    // if viewing has id, will assume that it's synced on the database
-    const saveMethod = viewing.id ? patchViewing : postViewing;
-    const viewingId = yield saveMethod(viewing);
-
-    // simulate server latency
-    // yield delay(500);
-
-    // yield put(change(ticketsFormName, viewingName, newViewing))
-    console.log('Saved viewing', viewingId);
-    yield put(ticketSubmitSucceeded(index, viewingId))
-  } catch (err) {
-    yield put(stopTicketSubmit(index))
-    console.error(err);
-  }
-}
-
 function* patchViewing(viewing) {
   const res = yield call(updateViewing, viewing);
-  yield viewing.id;
+  return viewing.id;
 }
 
 function* postViewing(viewing) {
   const createdViewing = yield call(saveNewViewing, viewing);
-  yield createdViewing.id;
+  return createdViewing.id;
+}
+
+function* saveViewing({
+  viewing,
+  index,
+  ticketName,
+}) {
+  try {
+    // if viewing has id, will assume that it's synced on the database
+    const saveMethod = viewing.id ? patchViewing : postViewing;
+    const viewingId = yield call(saveMethod, viewing);
+
+    yield put(ticketSubmitSucceeded(index, viewingId))
+    // add id from database to viewing
+    yield put(change(ticketsFormName, `${ticketName}.id`, viewingId));
+  } catch (err) {
+    yield put(stopTicketSubmit(index))
+    console.error(err);
+  }
 }
 
 function* watchStartTicketSubmit() {
