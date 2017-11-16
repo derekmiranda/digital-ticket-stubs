59d0284e433c89011d25ff16d9acd07654377b7f
Derek Miranda <derek.miranda.1992@gmail.com>
2017-11-16 11:28:40 -0800

    Added body parser

--- server/index.js
+++ server/index.js
@@ -1,11 +1,25 @@
 const path = require('path');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const searchRouter = require('./routers/searchRouter');
 const viewingsRouter = require('./routers/movieViewingsRouter');
 
 const port = process.env.PORT || 3000;
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// CORS
+app.use((req, res, next) => {
+	res.header({
+		'Access-Control-Allow-Origin': '*',
+		'Access-Control-Allow-Methods': 'GET, POST, PATCH, DELETE',
+		'Access-Control-Allow-Headers': 'Content-Type, Accepts',
+	});
+	next();
+})
 
 app.use(express.static('public'));
 app.use('/viewings', viewingsRouter);
