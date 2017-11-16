0ce3dcf6eb4109423012e2c94cce6facdad4bbf3
Derek Miranda <derek.miranda.1992@gmail.com>
2017-11-15 18:30:18 -0800

    Start on saga

--- client/sagas/index.js
+++ client/sagas/index.js
@@ -3,55 +3,53 @@ import { call, put, takeLatest, takeEvery } from 'redux-saga/effects';
 import { fetchViewings, saveNewViewing, removeViewing } from 'services/moviesApi';
 
 /* Adjust */
-import { FETCH_STARTED, CREATE_NEW_MOVIE, REQUEST_DELETE_MOVIE } from '../actions/types';
+import { START_TICKET_SUBMIT } from '../actions/types';
 import {
-  fetchSucceeded, 
-  addViewing, 
-  requestDeleteViewing, 
-  deleteViewing,
-  clearNewViewing,
+  ticketsFetchSucceeded, 
+  startTicketSubmit,
+  ticketSubmitSucceeded
 } from '../actions/creators';
 /*  */
 
-export function* fetchData(action) {
-  try {
-    const movies = yield call(fetchViewings);
-    yield put(fetchSucceeded(movies));
-  } catch (err) {
-    console.error(err);
-  }
-}
+// export function* fetchData(action) {
+//   try {
+//     const movies = yield call(fetchViewings);
+//     yield put(ticketsFetchSucceeded(movies));
+//   } catch (err) {
+//     console.error(err);
+//   }
+// }
 
-function* watchFetchData() {
-  yield takeLatest(FETCH_STARTED, fetchData);
-}
+// function* watchFetchData() {
+//   yield takeLatest(FETCH_STARTED, fetchData);
+// }
 
 export function* postViewing({ movie }) {
   try {
     const newViewing = yield call(saveNewViewing, movie);
-    yield put(addViewing(newViewing));
-    yield put(clearNewViewing())
+    yield put(startTicketSubmit(/* index */, newViewing));
+    yield put(ticketSubmitSucceeded(/* index */))
   } catch (err) {
     console.error(err);
   }
 }
 
 function* watchSaveNewViewing() {
-  yield takeEvery(CREATE_NEW_MOVIE, postViewing);
+  yield takeEvery(START_TICKET_SUBMIT, postViewing);
 }
 
-export function* clearViewing({ movie, movieIdx }) {
-  try {
-    yield call(removeViewing, movie);
-    yield put(deleteViewing(movieIdx));
-  } catch (err) {
-    console.error(err);
-  }
-}
+// export function* clearViewing({ movie, movieIdx }) {
+//   try {
+//     yield call(removeViewing, movie);
+//     yield put(deleteViewing(movieIdx));
+//   } catch (err) {
+//     console.error(err);
+//   }
+// }
 
-function* watchRequestDeleteViewing() {
-  yield takeEvery(REQUEST_DELETE_MOVIE, clearViewing);
-}
+// function* watchRequestDeleteViewing() {
+//   yield takeEvery(REQUEST_DELETE_MOVIE, clearViewing);
+// }
 
 export default function* rootSaga() {
   yield [watchFetchData(), watchSaveNewViewing(), watchRequestDeleteViewing()];
