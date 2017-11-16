238cc6b4805cc286637e0d0c8823aaad78aa111d
Derek Miranda <derek.miranda.1992@gmail.com>
2017-11-16 10:35:08 -0800

    Added test userId

--- client/containers/ViewingsFormContainer.js
+++ client/containers/ViewingsFormContainer.js
@@ -3,6 +3,8 @@ import { reduxForm } from 'redux-form';
 import Tickets from 'components/Tickets';
 import { ticketsFormName as formName } from 'client/constants';
 
+const getUserId = () => 1;
+
 const ViewingsFormContainer = reduxForm({
   form: formName,
   initialValues: {
@@ -14,6 +16,8 @@ const ViewingsFormContainer = reduxForm({
         title: 'Grave of the Fireflies'
       },
     ]
+    // for testing only
+    .map(viewing => ({ ...viewing, UserId: getUserId() }))
   },
   // onSubmit: (values) => console.log(values)
   onSubmit: () => new Promise((res, rej) => setTimeout(rej, 500, 'Reject'))
