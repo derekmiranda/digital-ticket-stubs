0d47d0c546f1a84c237ab27f262da6201530a399
Derek Miranda <derek.miranda.1992@gmail.com>
2017-11-16 11:29:49 -0800

    Fixed model association

--- models/movieViewing.js
+++ models/movieViewing.js
@@ -1,5 +1,5 @@
 'use strict';
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
   var MovieViewing = sequelize.define('MovieViewing', {
     title: {
       type: DataTypes.STRING,
@@ -10,17 +10,16 @@ module.exports = function(sequelize, DataTypes) {
     },
     venue: DataTypes.STRING,
     watchtime: DataTypes.DATE,
-  }, {
-    classMethods: {
-      associate: function(models) {
-        MovieViewing.belongsTo(models.User, {
-					onDelete: "CASCADE",
-					foreignKey: {
-						allowNull: false
-					}
-				});
+  })
+
+  MovieViewing.associate = function (models) {
+    MovieViewing.belongsTo(models.User, {
+      onDelete: "CASCADE",
+      foreignKey: {
+        allowNull: false
       }
-    }
-  });
+    });
+  }
+  
   return MovieViewing;
 };
