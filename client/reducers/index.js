ced83b960483bcb152a82f5f29549eefe5f55a4b
Derek Miranda <derek.miranda.1992@gmail.com>
2017-11-17 08:38:31 -0800

    Using reducer obj util

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,24 +1,30 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 
-import modalOpen from './modalOpen';
 import submittingTickets from './submittingTickets';
 import initialViewings from './initialViewings';
 import { reducerFromObj } from 'client/utils/reducerUtils';
 import {
   START_TICKETS_LOAD,
   TICKETS_LOAD_SUCCEEDED,
+  FOCUS_VIEWING,
+  LOADED_SEARCH_RESULTS,
 } from 'actions/types';
 
 const rootReducer = combineReducers({
   form: formReducer,
-  modalOpen,
   submittingTickets,
   initialViewings,
+  modalOpen: reducerFromObj({
+    [FOCUS_VIEWING]: (_, action) => action.viewingName,
+  }, null),
   loading: reducerFromObj({
     [START_TICKETS_LOAD]: () => true,
     [TICKETS_LOAD_SUCCEEDED]: () => false,
   }, false), 
+  searchResults: reducerFromObj({
+    [LOADED_SEARCH_RESULTS]: (_, action) => action.results,
+  }, null),
 })
 
 export default rootReducer;
