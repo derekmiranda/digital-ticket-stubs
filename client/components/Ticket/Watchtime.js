13eb6f018841917aa4c72ef4866377b99686f543
Derek Miranda <derek.miranda.1992@gmail.com>
2017-11-17 21:47:39 -0800

    Selects -> inputs

--- client/components/Ticket/Watchtime.js
+++ client/components/Ticket/Watchtime.js
@@ -8,51 +8,28 @@ import {
 	createDescendingOptionsRange
 } from './optionsRangeFns';
 
-const createSelectRendererWithOptions = (options) => ({ input, meta: { pristine } }) => {
-	// setting onBlur and onFocus to null since interfering w/ click events
-	// source: https://github.com/erikras/redux-form/issues/860  
-	return (
-		<select {...input} onBlur={null} onFocus={null}>
-			{options}
-		</select>
-	)
-}
-
-const createTimeSelectField = (name, options) => {
-	const renderSelect = createSelectRendererWithOptions(options);
+const createTimeInputField = (name, normalize) => {
 	return (
 		<Field
 			name={name}
-			component={renderSelect}
-			type='select'
+			component='input'
+			type='text'
+			normalize={normalize}
 		/>
 	)
 }
 
 const Watchtime = ({ name: watchtimeName }) => {
-	const monthSelect = createTimeSelectField(
-		'month',
-		createAscendingOptionsRange(1, 12, 'Month')
-	)
-
-	const daySelect = createTimeSelectField(
-		'day',
-		createAscendingOptionsRange(1, 30, 'Day')
-	)
-	
-  const currYear = new Date().getFullYear();
-	const yearOptions = createDescendingOptionsRange(currYear, 1920, 'Year');
-	const yearSelect = createTimeSelectField(
-		'year',
-		yearOptions
-	)
+	const monthInput = createTimeInputField('month', () => {}); 
+	const dayInput = createTimeInputField('day', () => {});
+	const yearInput = createTimeInputField('year', () => {});
 
 	return (
 		<FormSection name={watchtimeName} className='watchtime'>
 			<h3>Watch Time</h3>
-			{monthSelect}
-			{daySelect}
-			{yearSelect}
+			{monthInput}
+			{dayInput}
+			{yearInput}
 		</FormSection>
 	)
 }
