115532c56ccf242d4f28da7ea636fca1bac73f6a
Derek Miranda <derek.miranda.1992@gmail.com>
2017-11-26 12:30:26 -0800

    Move index regex fn to utils

--- client/utils/formUtils.js
+++ client/utils/formUtils.js
@@ -1,2 +1,7 @@
 // get flat array of all fields under a Redux Form field
 export const getTicketFields = (fieldNames, ticketName) => fieldNames.filter(name => name.includes(ticketName));
+
+export const getViewingsIndex = (field) => {
+  const match = field.match(/^viewings\[(\d+)\]/);
+  return match && match[1];
+} 
