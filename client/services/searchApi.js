6674654cf8de4dc2e6b03eeb839d70a60352e8dc
Derek Miranda <derek.miranda.1992@gmail.com>
2017-12-09 18:38:17 -0800

    Don't search when no title

--- client/services/searchApi.js
+++ client/services/searchApi.js
@@ -1,6 +1,7 @@
 const { SEARCH_API_URL } = process.env;
 
 export async function searchForTitle(title) {
+  if (!title.trim()) return;
   try {
     const res = await fetch(`${SEARCH_API_URL}/${title}`)
     const { results } = await res.json();
