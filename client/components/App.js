0c29ea852c3c8b3f1811e53b8dfe74ffa37cf018
Derek Miranda <derek.miranda@ff0000.com>
2017-12-11 09:28:49 -0800

    Lowered padding for mobile

--- client/components/App.js
+++ client/components/App.js
@@ -4,9 +4,15 @@ import styled from 'styled-components';
 import ViewingsFormContainer from 'containers/ViewingsFormContainer';
 import Attribution from './Attribution';
 import { bgColor, mainTextColor } from 'constants';
+import { forMobile } from 'client/utils/styleUtils';
 
 const Container = styled.main`
 	padding: 3em;
+
+	${forMobile(`
+		padding: 1.5em;
+	`)}
+	
 	height: 100%;
 	background-color: ${bgColor};
 	color: ${mainTextColor};
