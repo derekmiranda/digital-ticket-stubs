fcc43c969b052857f239ef667090476eaa8cafd4
Derek Miranda <derek.miranda.1992@gmail.com>
2017-12-11 19:44:29 -0800

    Using vw units to maintain Ticket aspect ratio

--- client/components/styled/StyledTicket.js
+++ client/components/styled/StyledTicket.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 import Ticket from 'components/Ticket'
 import {
 	forMobile,
+	forTablet,
 	forDesktop,
 } from 'client/utils/styleUtils';
 
@@ -12,30 +13,20 @@ const height = 225;
 const mobileWidth = 320;
 const mobileHeight = 180;
 
+const generateVwDims = (mediaQueryGen, vwWidth) => mediaQueryGen(`
+	width: ${vwWidth}vw;
+	height: calc(${vwWidth}vw * 9 / 16);
+`)
+
 const StyledTicket = styled(Ticket)`
 	box-sizing: border-box;
-	width: 100%;
+
+	${generateVwDims(forMobile, 80)}
+	${generateVwDims(forTablet, 40)}
+	${generateVwDims(forDesktop, 27)}
+
 	margin: 0;
-
-	/* 
-		Aspect ratio using pseudo elements
-		https://css-tricks.com/aspect-ratio-boxes/#article-header-id-6
-	*/
-	&::before {
-		content: "";
-		width: 1px;
-		margin-left: -1px;
-		float: left;
-		height: 0;
-		padding-top: 56.25%;
-	}
-	
-	&::after { /* to clear float */
-		content: "";
-		display: table;
-		clear: both;
-	}
-
+	margin-bottom: 2em;
 	outline: .25em solid rgba(0,0,0,0.5);
 	opacity: ${props => props.viewing.id ? 1 : .75};
 	background-color: rgba(0,0,0,0);
