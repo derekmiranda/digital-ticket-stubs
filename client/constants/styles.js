93aae0f595fad84fce13e79ff4d7fafc8ccb00e4
Derek Miranda <derek.miranda@ff0000.com>
2017-12-13 07:49:36 -0800

    Lighter bg

--- client/constants/styles.js
+++ client/constants/styles.js
@@ -1,4 +1,4 @@
-export const bgColor = '#fffff0'
+export const bgColor = '#fffffb'
 export const mainTextColor = '#555'
 export const tmdbPrimaryColor = '#01d277'
 export const ticketMargin = '.25em'
