69fdab902287ec30195b175b1036c65cabb1cfbc
Derek Miranda <derek.miranda@ff0000.com>
2017-12-14 07:45:36 -0800

    Clear results on ticket deletion

--- client/reducers/searchResults.js
+++ client/reducers/searchResults.js
@@ -1,27 +1,40 @@
-import { actionTypes } from 'redux-form';
+import {
+	actionTypes
+} from 'redux-form';
 
-import { LOADED_SEARCH_RESULTS, SORT_TICKETS } from 'actions/types';
-import { getViewingsIndex } from 'client/utils/formUtils';
+import {
+	LOADED_SEARCH_RESULTS,
+	SORT_TICKETS,
+	TICKET_DELETE_SUCCEEDED
+} from 'actions/types';
+import {
+	getViewingsIndex
+} from 'client/utils/formUtils';
 
-const { FOCUS } = actionTypes;
+const {
+	FOCUS
+} = actionTypes;
 
 const searchResults = (state = {}, action = {}) => {
 	switch (action.type) {
+		// clearing actions
+		case TICKET_DELETE_SUCCEEDED:
+		case SORT_TICKETS:
+			return {};
 		case LOADED_SEARCH_RESULTS:
 			return {
 				...state,
 				[action.index]: action.results,
 			}
-		case SORT_TICKETS:
-			return {};
-		case FOCUS: {
-			const focusedIndex = getViewingsIndex(action.meta.field);
-			if (isNaN(focusedIndex)) {
-				return {};
+		case FOCUS:
+			{
+				const focusedIndex = getViewingsIndex(action.meta.field);
+				if (isNaN(focusedIndex)) {
+					return {};
+				}
+				// clear results if focused on different ticket
+				return state[focusedIndex] ? state : {};
 			}
-			// clear results if focused on different ticket
-			return state[focusedIndex] ? state : {};
-		}
 		default:
 			return state;
 	}
