8cee3ef3e04dc09f9504fe8bee82c3eb36beb03b
Derek Miranda <derek.miranda@ff0000.com>
2017-12-14 07:53:32 -0800

    Increased rotation

--- client/meta/createBaseViewing.js
+++ client/meta/createBaseViewing.js
@@ -1,7 +1,7 @@
 import getFormId from './getFormId'
 import { getUserId } from '../auth'
 
-const degRange = 2
+const degRange = 4
 const randAngle = () => Math.random()*degRange - degRange / 2
 
 export default () => ({
