77213839cf9332f8393030a996372ba847081678
Derek Miranda <derek.miranda.1992@gmail.com>
2018-01-01 20:25:23 -0800

    Search on focus

--- client/sagas/handleMovieSearch.js
+++ client/sagas/handleMovieSearch.js
@@ -6,8 +6,10 @@ import { searchForTitle } from 'services/searchApi';
 import { loadedSearchResults } from 'actions/creators';
 import { getViewingsIndex } from 'client/utils/formUtils';
 import { searchWaitTime, ticketsFormName } from 'client/constants';
+import { store } from 'client'
 
 const { CHANGE, FOCUS }  = actionTypes;
+const getFieldVal = formValueSelector(ticketsFormName) 
 
 const changeActionWithMeta = (action) => (
   action.type === CHANGE && action.meta
@@ -15,12 +17,12 @@ const changeActionWithMeta = (action) => (
 const titleChangeAction = (action) => (
   changeActionWithMeta(action) && action.meta.field.includes('title')
 )
-// const titleChangeOrFocus = (action) => {
-//   return (
-//     titleChangeAction(action) ||
-//     action.type === FOCUS && 
-//   )
-// }
+const titleChangeOrFocus = (action) => {
+  return (
+    titleChangeAction(action) ||
+    action.type === FOCUS && action.meta.field.includes('title')
+  )
+}
 
 function* searchThruPayload(field, payload) {
   const results = yield searchForTitle(payload);
@@ -28,16 +30,27 @@ function* searchThruPayload(field, payload) {
   yield put(loadedSearchResults(index, results));
 }
 
-function* waitToSearch({ meta: { field }, payload }, arg2) {
+function* searchOnFocus(field) {
+  const titleVal = getFieldVal(store.getState(), field);
+  if (!titleVal) return;
+
+  const results = yield searchForTitle(titleVal);
+  const index = getViewingsIndex(field);
+  yield put(loadedSearchResults(index, results));
+}
+
+function* waitToSearch({ type, meta: { field }, payload }) {
   yield call(delay, searchWaitTime);
 
   if (payload && payload.trim()) {
     yield searchThruPayload(field, payload)
+  } else if (type === FOCUS) {
+    yield searchOnFocus(field)
   }
 }
 
 function* handleMovieSearch() {
-  yield takeLatest(titleChangeAction, waitToSearch);
+  yield takeLatest(titleChangeOrFocus, waitToSearch);
 }
 
 export default handleMovieSearch;
