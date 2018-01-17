3f6f1b6e37ff1f4478c411f800b0068e171dec00
Derek Miranda <derek.miranda@ff0000.com>
2018-01-17 08:26:17 -0800

    Set App to home in dev environments

--- client/components/App.js
+++ client/components/App.js
@@ -23,20 +23,26 @@ const Container = styled.main`
 	overflow: auto;
 `
 
+const renderRegisterPage = () => (
+	<div>
+		<Link to='/stubs'>
+			<h2>Go to App</h2>
+		</Link>
+		<RegisterPage />
+	</div>
+)
+
 const RouterViewingsFormContainer = withRouter(ViewingsFormContainer)
 
 const App = () => (
 	<Router>
 		<Container>
 			<h1>Digital Ticket Stubs</h1>
-			<Route exact path='/' render={() => (
-				<div>
-					<Link to='/stubs'>
-						<h2>Go to App</h2>
-					</Link>
-					<RegisterPage />
-				</div>
-			)} />
+			<Route exact path='/' render={
+				process.env.NODE_ENV === 'development'
+					? RouterViewingsFormContainer
+					: renderRegisterPage
+			} />
 			<Route path='/stubs' component={RouterViewingsFormContainer} />
 			<Attribution />
 		</Container>
