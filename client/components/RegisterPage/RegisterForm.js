dbf1590481bf0fa65a4d86c2dacd08a8ae499f81
Derek Miranda <derek.miranda.1992@gmail.com>
2018-01-21 12:18:34 -0800

    -Added password fields -Input labels on diff line w/ <br>

--- client/components/RegisterPage/RegisterForm.js
+++ client/components/RegisterPage/RegisterForm.js
@@ -1,37 +1,56 @@
 import React from 'react'
-import { Field } from 'redux-form'
+import {Field} from 'redux-form'
+import styled from 'styled-components'
 
-const renderInput = ({ label, input }) => (
-  <div>
-    {label}<input {...input} />
-  </div>
+const renderInput = ({label, input, type}) => (
+  <label style={{
+    display: 'inline-block',
+    textAlign: 'left',
+  }}>
+    {label}
+    <br display='none' />
+    <input {...input} type={ type || 'text' }/>
+  </label>
 )
 
-const RegisterForm = ({ handleSubmit }) => (
-	<form>
-    <h3>Register</h3>
-    <Field
-      name='username'
-      component={renderInput}
-      label='Username'
-    />
-    <Field
-      name='email'
-      component={renderInput}
-      label='Email'
-    />
-    <Field
-      name='firstName'
-      component={renderInput}
-      label='First Name'
-    />
-    <Field
-      name='lastName'
-      component={renderInput}
-      label='Last Name'
-    />
-		<button type='button' onClick={handleSubmit}>Submit</button>
-	</form>
+const StyledForm = styled.form`
+  padding: 2em;
+
+
+`
+
+const RegisterForm = ({handleSubmit}) => (
+  <StyledForm>
+    <h2 style={{ marginTop: 0, paddingTop: 0 }}>Register</h2>
+    <fieldset style={{
+      border: 0,
+      padding: 0,
+    }}>
+      <Field name='firstName' component={renderInput} label='First Name'/>
+      <Field name='lastName' component={renderInput} label='Last Name'/>
+    </fieldset>
+    <div>
+      <Field name='username' component={renderInput} label='Username'/>
+    </div>
+    <div>
+      <Field name='email' component={renderInput} label='Email'/>
+    </div>
+    <div>
+      <Field
+        name='password'
+        component={renderInput}
+        label='Password'
+        type='password'/>
+    </div>
+    <div>
+      <Field
+        name='password_confirm'
+        component={renderInput}
+        label='Confirm Password'
+        type='password'/>
+    </div>
+    <button type='button' onClick={handleSubmit}>Submit</button>
+  </StyledForm>
 )
 
 export default RegisterForm
