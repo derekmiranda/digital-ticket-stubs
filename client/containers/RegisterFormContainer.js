cca256929410f53afa2faae8f11dc7cb7107deec
Derek Miranda <derek.miranda@ff0000.com>
2018-01-23 07:44:40 -0800

    AsyncBlurFields

--- client/containers/RegisterFormContainer.js
+++ client/containers/RegisterFormContainer.js
@@ -3,11 +3,13 @@ import { connect } from 'react-redux';
 
 import RegisterForm from 'components/RegisterPage/RegisterForm'
 import { registerFormName as formName } from 'client/constants'
-import { validateRegisterForm } from 'validators'
+import { validateRegisterForm, asyncValidateRegisterForm } from 'validators'
 
 const RegisterFormContainer = reduxForm({
   form: formName,
   validate: validateRegisterForm,
+  // asyncValidate: asyncValidateRegisterForm,
+  asyncBlurFields: ['username', 'email'],
 })(RegisterForm)
 
 export default RegisterFormContainer
