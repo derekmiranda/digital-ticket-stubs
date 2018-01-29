2b64af8334b93de7105b115c0cd47bfd112830d5
Derek Miranda <derek.miranda.1992@gmail.com>
2018-01-28 22:37:12 -0800

    Using format JSON fn for other JSON responses

--- server/routers/usersRouter.js
+++ server/routers/usersRouter.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const usersController = require('../controllers/usersController');
+const { formattedJSONResponse } = require('./utils');
 
 const usersRouter = express.Router();
 
 usersRouter.post('/check', async (req, res, next) => {
   const user = req.body;
   const errors = await usersController.checkUser(user)
-  res.status(422).send(errors)
+  return formattedJSONResponse(res.status(422), errors)
 });
 
 usersRouter.post('/create', async (req, res, next) => {
@@ -15,17 +16,19 @@ usersRouter.post('/create', async (req, res, next) => {
 
   switch (result.error) {
     case 'database':
-      return res.status(422).send(result) 
+      return formattedJSONResponse(res.status(422), result) 
     case 'server':
       throw result
     default:
-      res
-        .status(201)
-        .set({
-          'Location': `${req.protocol}//:${req.hostname}/api/viewings`
-        })
+      formattedJSONResponse(
+        res
+          .status(201)
+          .set({
+            'Location': `${req.protocol}//:${req.hostname}/api/viewings`
+          }),
+        result
+      )
         // TODO: send token
-        .send(result)
   }
 });
 
