3aa1e0d82f27c2680e2a7be99b90367ec0cd94f0
Derek Miranda <derek.miranda.1992@gmail.com>
2018-01-29 19:43:26 -0800

    Fleshed out token validator

--- server/tokens/index.js
+++ server/tokens/index.js
@@ -1,35 +1,72 @@
 const jwt = require('jsonwebtoken')
+const db = require('../../models')
+
+const { User } = db
 const { JWT_SECRET, JWT_EXPIRY, JWT_REFRESH_TIME } = process.env
 
-module.exports = {
-	createToken({ user, host }) {
-		const now = Date.now() / 1000
-		const expiry = +JWT_EXPIRY
-		const payload = {
-			iss: host,
-			sub: user.username,
-			exp: now + expiry,
-			iat: now,
-			email: user.email,
-			role: 'user'
-		}
+function createToken({ user, host }) {
+	const now = Date.now() / 1000
+	const expiry = +JWT_EXPIRY
+	const payload = {
+		iss: host,
+		sub: user.username,
+		email: user.email,
+		role: 'user'
+	}
 
-		return new Promise((resolve, reject) => {
-			jwt.sign(payload, JWT_SECRET, (err, token) => {
-				if (err) reject(err)
-				resolve(token)
-			})
+	return new Promise((resolve, reject) => {
+		jwt.sign(payload, JWT_SECRET, {
+			expiresIn: JWT_EXPIRY,
+		}, (err, token) => {
+			if (err) reject(err)
+			resolve(token)
 		})
-	},
+	})
+}
 
-	checkTokenExpiry(expiry) {
-		const expiry_ms = expiry * 1000
-		return Date.now() >= expiry_ms
-	},
+function hasExpiryPast(expiry) {
+	const expiry_ms = expiry * 1000
+	const expired = Date.now() >= expiry_ms
+	resolve(expired)
+}
 
-	shouldRefreshToken(issueTime) {
-		const refreshTime = +JWT_REFRESH_TIME
-		const refreshThreshold = (issueTime + refreshTime) * 1000
-		return Date.now() >= refreshThreshold
-	},
+async function checkIfUserExists(username) {
+	try {
+		const user = await User.findOne({
+			where: { username }
+		})
+		return !!user
+	} catch (err) {
+		throw err
+	}
+}
+
+function isTokenValid(token) {
+	return new Promise((resolve, reject) => {
+		jwt.verify(token, JWT_SECRET, (err, payload) => {
+			if (err) return reject(err)
+			if (hasExpiryPast(payload.exp)) return reject("Expired token")
+
+			resolve(checkIfUserExists(payload.sub))
+		})
+	})
+		.then((userExists) => {
+			if (userExists) {
+				return true
+			}	else {
+				throw "User doesn't exist"
+			}
+		})
+}
+
+function shouldRefreshToken(issueTime) {
+	const refreshTime = +JWT_REFRESH_TIME
+	const refreshThreshold = (issueTime + refreshTime) * 1000
+	return Date.now() >= refreshThreshold
+}
+
+module.exports = {
+	createToken,
+	isTokenValid,
+	shouldRefreshToken,
 }
