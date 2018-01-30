07760683089f4442b0ba211123b06d80d722265f
Derek Miranda <derek.miranda.1992@gmail.com>
2018-01-29 19:55:32 -0800

    Start on cookie upon register test

--- test/server/register.spec.js
+++ test/server/register.spec.js
@@ -1,7 +1,29 @@
+// load environment vars
+const {
+  USER_API_URL
+} = require('dotenv').load(
+  { path: path.resolve(__dirname, `../../config/${process.env.NODE_ENV}.env`) }
+);
+
 import test from 'ava'
 import request from 'supertest'
 
 import app from '../../server'
+import { dbSetup } from 'utils/tests/db';
 
-test.todo('Invalid user body receives a 422 error from user API')
-test.todo('Valid user body receives 200 from user API')
+
+dbSetup(test);
+
+test.serial("Successful user registration provides token as cookie", async t => {
+  try {
+    await request(app)
+      .post(USER_API_URL, {
+        username: 'sample',
+        email: 'sample@sample.com',
+        passHash: 'sample_password',
+      })
+      .expect(/* cookie */)
+  } catch (err) {
+    throw err
+  }
+})
