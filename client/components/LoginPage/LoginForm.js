0f604d605e3708919a982a717be37fef878b649f
Derek Miranda <derek.miranda.1992@gmail.com>
2018-02-04 20:36:46 -0800

    Can't submit till values filled in

--- client/components/LoginPage/LoginForm.js
+++ client/components/LoginPage/LoginForm.js
@@ -3,12 +3,14 @@ import {Field} from 'redux-form'
 import {renderInput} from '../AuthComponents'
 import {Link} from 'react-router-dom'
 
+import {isRequired} from '../../validators'
+
 const inputStyle = {
   boxSizing: 'border-box',
   width: '25em'
 }
 
-const LoginForm = ({handleSubmit}) => {
+const LoginForm = ({handleSubmit, valid}) => {
   return (
     <form onSubmit={handleSubmit}>
       <h2 style={{
@@ -20,6 +22,7 @@ const LoginForm = ({handleSubmit}) => {
         label='Username'
         component={renderInput}
         inputStyle={inputStyle}
+        validate={isRequired}
       />
       <br display='none'/>
       <Field
@@ -28,9 +31,10 @@ const LoginForm = ({handleSubmit}) => {
         type='password'
         component={renderInput}
         inputStyle={inputStyle}
+        validate={isRequired}
       />
       <br display='none'/>
-      <button type='submit'>Submit</button>
+      <button type='submit' disabled={!valid}>Submit</button>
       <Link to='/'>
         <p>No account yet?</p>
       </Link>
