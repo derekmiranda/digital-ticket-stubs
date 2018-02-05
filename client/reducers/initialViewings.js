341f0f66779906fc38dda8f7567562a625d0f63c
Derek Miranda <derek.miranda@ff0000.com>
2018-02-05 08:11:56 -0800

    Initialize w/ blank viewing if none loaded

--- client/reducers/initialViewings.js
+++ client/reducers/initialViewings.js
@@ -1,10 +1,13 @@
 import { TICKETS_LOAD_SUCCEEDED } from 'actions/types';
+import createBaseViewing from '../meta/createBaseViewing'
 
 const initialViewings = (state = {}, action = {}) => {
 	switch (action.type) {
 		case TICKETS_LOAD_SUCCEEDED:
 			return {
-				viewings: action.loadedTickets,
+				viewings: action.loadedTickets.length
+					? action.loadedTickets
+					: [createBaseViewing()],
 			}
 		default:
 			return state;
