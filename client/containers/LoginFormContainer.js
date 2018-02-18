88327e82c56600785ac98f8d867f070a62b244a9
Derek Miranda <derek.miranda.1992@gmail.com>
2018-02-18 15:03:12 -0800

    SPA redirect on successful login
    - HOC for working w/ Router history

--- client/containers/LoginFormContainer.js
+++ client/containers/LoginFormContainer.js
@@ -1,11 +1,30 @@
-import { reduxForm } from 'redux-form';
+import React, { Component } from 'react'
+import { reduxForm } from 'redux-form'
 
 import LoginForm from 'components/LoginPage/LoginForm'
 import { loginFormName as formName } from 'client/constants'
-import { loginUser } from '../services/userApi'
+import { submitUser } from '../services/userApi'
+
+class LoginFormWithRouter extends Component {
+  submitForm(user) {
+    return Promise.resolve(submitUser(user))
+      .then(result => {
+        this.props.history.push('/')
+      })
+      .catch(err => {
+        throw new SubmissionError({
+          _error: 'Login failed, please try again.'
+        })
+      })
+  }
+
+  render() {
+    return <LoginForm {...this.props} submitForm={this.submitForm.bind(this)} />
+  }
+}
 
 const LoginFormContainer = reduxForm({
-  form: formName,
-})(LoginForm)
+  form: formName
+})(LoginFormWithRouter)
 
 export default LoginFormContainer
