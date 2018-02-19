37a7f79d8e729e6203e85c6a3aae420ea5ed9926
Derek Miranda <derek.miranda.1992@gmail.com>
2018-02-18 16:00:42 -0800

    Ok, I do need to respond after Passport authenticates

--- server/passport/verify.js
+++ server/passport/verify.js
@@ -1,37 +1,37 @@
-const { User } = require("../../models");
+const { User } = require('../../models')
 
 exports.verifyLogin = async function(username, passHash, done) {
   try {
-    const user = await User.find({ where: { username } });
+    const user = await User.find({ where: { username } })
     if (!user) {
       return done(null, false, {
         message: "User doesn't exist."
-      });
+      })
     }
-    const verified = await user.verifyPassHash(passHash);
+    const verified = await user.verifyPassHash(passHash)
     if (!verified) {
       return done(null, false, {
         message: "Username/password combination doesn't exist"
-      });
+      })
     }
-    return done(null, user);
+    return done(null, user)
   } catch (err) {
-    return done(err);
+    return done(err)
   }
-};
+}
 
 exports.verifyRegister = async function(req, username, passHash, done) {
   try {
     const user = req.body
-    const result = await User.create(user);
-    return done(null, result);
+    const result = await User.create(user)
+    return done(null, result)
   } catch (err) {
-    if (err.name && err.name.includes("Sequelize")) {
+    if (err.name && err.name.includes('Sequelize')) {
       console.error(err)
       return done(null, false, {
-        message: "Database error"
-      });
+        message: 'Error saving user, please try again.'
+      })
     }
     return done(err)
   }
-};
+}
--- server/routers/usersRouter.js
+++ server/routers/usersRouter.js
@@ -1,31 +1,36 @@
-const express = require("express");
-const passport = require("passport");
-const usersController = require("../controllers/usersController");
-const { createToken } = require("../tokens");
-const { formattedJSONResponse } = require("./utils");
+const express = require('express')
+const passport = require('passport')
+const usersController = require('../controllers/usersController')
+const { createToken } = require('../tokens')
+const { formattedJSONResponse } = require('./utils')
 
-const { APP_SERVER_ORIGIN } = process.env;
-const usersRouter = express.Router();
+const { APP_SERVER_ORIGIN } = process.env
+const usersRouter = express.Router()
 
 // Response Headers
 usersRouter.use((req, res, next) => {
   res.header({
     // CORS
-    "Access-Control-Allow-Origin": APP_SERVER_ORIGIN,
-    "Access-Control-Allow-Credentials": "true",
-    "Access-Control-Allow-Methods": "GET, POST, PATCH, DELETE",
-    "Access-Control-Allow-Headers": "Content-Type, Accepts"
-  });
-  next();
-});
+    'Access-Control-Allow-Origin': APP_SERVER_ORIGIN,
+    'Access-Control-Allow-Credentials': 'true',
+    'Access-Control-Allow-Methods': 'GET, POST, PATCH, DELETE',
+    'Access-Control-Allow-Headers': 'Content-Type, Accepts'
+  })
+  next()
+})
 
-usersRouter.post("/check", async (req, res, next) => {
-  const user = req.body;
-  const errors = await usersController.checkUser(user);
-  return formattedJSONResponse(res.status(422), errors);
-});
+usersRouter.post('/check', async (req, res, next) => {
+  const user = req.body
+  const errors = await usersController.checkUser(user)
+  return formattedJSONResponse(res.status(422), errors)
+})
 
-usersRouter.post("/login", passport.authenticate('login'));
-usersRouter.post("/register", passport.authenticate('register'));
+usersRouter.post('/login', passport.authenticate('login'), (req, res) => {
+  return res.send('User logged in')
+})
 
-module.exports = usersRouter;
+usersRouter.post('/register', passport.authenticate('register'), (req, res) => {
+  return res.status(201).send('User created')
+})
+
+module.exports = usersRouter
