12900915956ec253244da9498946983e48f43c06
Derek Miranda <derek.miranda.1992@gmail.com>
2018-02-19 11:47:54 -0800

    Protected viewings API

--- server/routers/movieViewingsRouter.js
+++ server/routers/movieViewingsRouter.js
@@ -1,28 +1,38 @@
 const express = require('express');
+const log = require('debug')('stubs:viewings:router')
 
 const { makeJSONResponseMiddleware } = require('./utils');
 const movieViewingsController = require('../controllers/movieViewingsController');
 
-const movieViewingsRouter = express.Router();
+const router = express.Router();
 
-movieViewingsRouter.get('/', makeJSONResponseMiddleware(movieViewingsController.getMovieViewings));
+// check if user authenticated
+router.use((req, res, next) => {
+  log(req.sessionID)
+  if (!req.session.passport) {
+    return res.status(401).send('Unauthorized')
+  }
+  next()
+})
 
-movieViewingsRouter.post('/', (req, res, next) => {
+router.get('/', makeJSONResponseMiddleware(movieViewingsController.getMovieViewings));
+
+router.post('/', (req, res, next) => {
   const movieViewing = req.body;
   const middleware = makeJSONResponseMiddleware(movieViewingsController.addMovieViewing, movieViewing);
   return middleware(req, res, next);
 });
 
-movieViewingsRouter.patch('/', (req, res, next) => {
+router.patch('/', (req, res, next) => {
   const movieViewings = req.body;
   const middleware = makeJSONResponseMiddleware(movieViewingsController.updateMovieViewings, movieViewings);
   return middleware(req, res, next);
 });
 
-movieViewingsRouter.delete('/:id', (req, res, next) => {
+router.delete('/:id', (req, res, next) => {
   const { id } = req.params;
   const middleware = makeJSONResponseMiddleware(movieViewingsController.removeMovieViewing, id);
   return middleware(req, res, next);
 });
 
-module.exports = movieViewingsRouter;
+module.exports = router;
