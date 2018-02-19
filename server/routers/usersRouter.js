d0514e428aa5ddcf47bd8b98bfde3c336325afd3
Derek Miranda <derek.miranda.1992@gmail.com>
2018-02-19 08:44:37 -0800

    Removed token stuff

--- server/routers/usersRouter.js
+++ server/routers/usersRouter.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const passport = require('passport')
 const usersController = require('../controllers/usersController')
-const { createToken } = require('../tokens')
 const { formattedJSONResponse } = require('./utils')
 
 const { APP_SERVER_ORIGIN } = process.env
