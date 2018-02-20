c144f36e86c3cab35c7efe3985f3f0bd5a9c3eac
Derek Miranda <derek.miranda.1992@gmail.com>
2018-02-19 21:13:11 -0800

    Separated payload validation func

--- server/tokens/index.js
+++ server/tokens/index.js
@@ -5,68 +5,77 @@ const { User } = db
 const { JWT_SECRET, JWT_EXPIRY, JWT_REFRESH_TIME } = process.env
 
 function createToken({ user, host }) {
-	const now = Date.now() / 1000
-	const expiry = +JWT_EXPIRY
-	const payload = {
-		iss: host,
-		sub: user.username,
-		email: user.email,
-		role: 'user'
-	}
+  const now = Date.now() / 1000
+  const expiry = +JWT_EXPIRY
+  const payload = {
+    iss: host,
+    sub: user.username,
+    role: 'user'
+  }
 
-	return new Promise((resolve, reject) => {
-		jwt.sign(payload, JWT_SECRET, {
-			expiresIn: JWT_EXPIRY,
-		}, (err, token) => {
-			if (err) reject(err)
-			resolve(token)
-		})
-	})
+  return new Promise((resolve, reject) => {
+    jwt.sign(
+      payload,
+      JWT_SECRET,
+      {
+        expiresIn: JWT_EXPIRY
+      },
+      (err, token) => {
+        if (err) reject(err)
+        resolve(token)
+      }
+    )
+  })
 }
 
 function hasExpiryPast(expiry) {
-	const expiry_ms = expiry * 1000
-	const expired = Date.now() >= expiry_ms
-	resolve(expired)
+  const expiry_ms = expiry * 1000
+  const expired = Date.now() >= expiry_ms
+  resolve(expired)
 }
 
 async function checkIfUserExists(username) {
-	try {
-		const user = await User.findOne({
-			where: { username }
-		})
-		return !!user
-	} catch (err) {
-		throw err
-	}
+  try {
+    const user = await User.findOne({
+      where: { username }
+    })
+    return !!user
+  } catch (err) {
+    throw err
+  }
 }
 
 function isTokenValid(token) {
-	return new Promise((resolve, reject) => {
-		jwt.verify(token, JWT_SECRET, (err, payload) => {
-			if (err) return reject(err)
-			if (hasExpiryPast(payload.exp)) return reject("Expired token")
+  return new Promise((resolve, reject) => {
+    jwt.verify(token, JWT_SECRET, (err, payload) => {
+      if (err) return reject(err)
+      resolve(isPayloadValid(payload))
+    })
+  })
+}
 
-			resolve(checkIfUserExists(payload.sub))
-		})
-	})
-		.then((userExists) => {
-			if (userExists) {
-				return true
-			}	else {
-				throw "User doesn't exist"
-			}
-		})
+function isPayloadValid(payload) {
+  return new Promise((resolve, reject) => {
+    if (hasExpiryPast(payload.exp)) return reject('Expired token')
+    resolve(checkIfUserExists(payload.sub))
+  }).then(userExists => {
+    if (userExists) {
+      return true
+    } else {
+      throw "User doesn't exist"
+    }
+  })
 }
 
 function shouldRefreshToken(issueTime) {
-	const refreshTime = +JWT_REFRESH_TIME
-	const refreshThreshold = (issueTime + refreshTime) * 1000
-	return Date.now() >= refreshThreshold
+  const refreshTime = +JWT_REFRESH_TIME
+  const refreshThreshold = (issueTime + refreshTime) * 1000
+  return Date.now() >= refreshThreshold
 }
 
 module.exports = {
-	createToken,
+  createToken,
 	isTokenValid,
-	shouldRefreshToken,
+	isPayloadValid,
+  shouldRefreshToken
 }
