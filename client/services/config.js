7296cabb1d7f28a9c9c84f90be5089edf5ba62b4
Derek Miranda <derek.miranda.1992@gmail.com>
2018-02-19 21:15:57 -0800

    For requests to viewings API, set Authorization to access token w/ Bearer scheme

--- client/services/config.js
+++ client/services/config.js
@@ -1,3 +1,8 @@
+import { getToken } from '../auth'
+
 export const authConfig = {
-  credentials: process.env.NODE_ENV === 'production' ? 'same-origin' : 'include'
+  credentials: process.env.NODE_ENV === 'production' ? 'same-origin' : 'include',
+  headers: {
+    "Authorization": `Bearer ${getToken()}`
+  }
 }
