cad5cc1beff4646cfa71413ce7d34a20ad1dcceb
Derek Miranda <derek.miranda.1992@gmail.com>
2018-02-19 21:10:55 -0800

    LocalStorage methods

--- client/auth/index.js
+++ client/auth/index.js
@@ -1,2 +1,15 @@
-export const getUserId = () => 1;
-export const isAuthenticated = () => false;
+import { ACCESS_TOKEN } from '../constants'
+
+export const isAuthenticated = () => getToken()
+
+export function getToken() {
+  return localStorage.getItem(ACCESS_TOKEN)
+}
+
+export function saveToken(token) {
+  return localStorage.setItem(ACCESS_TOKEN, token)
+}
+
+export function clearToken() {
+  return localStorage.removeItem(ACCESS_TOKEN)
+}
