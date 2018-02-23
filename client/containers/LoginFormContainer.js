3cd19b73a9ba119d64b174d907743ef4421bb9f2
Derek Miranda <derek.miranda.1992@gmail.com>
2018-02-23 14:32:51 -0800

    Using action creator passed in

--- client/containers/LoginFormContainer.js
+++ client/containers/LoginFormContainer.js
@@ -8,7 +8,7 @@ import { loginUser } from '../services/userApi'
 
 class LoginFormWithRouter extends Component {
   submitForm(user) {
-    return Promise.resolve(loginUser(user))
+    return Promise.resolve(this.props.startUserLogin(user))
       .then(result => {
         this.props.history.push('/')
       })
