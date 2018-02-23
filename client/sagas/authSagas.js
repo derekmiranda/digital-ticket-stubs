c8686578dee1503264f7315048b4f50d80698e2a
Derek Miranda <derek.miranda.1992@gmail.com>
2018-02-23 14:21:44 -0800

    Submission sagas
    - also saving token in saga instead of directly in request handler

--- client/sagas/authSagas.js
+++ client/sagas/authSagas.js
@@ -1,13 +1,44 @@
-import { call, takeLatest, put } from 'redux-saga/effects';
-import { delay } from 'redux-saga';
+import { call, takeLatest, put } from 'redux-saga/effects'
+import { delay } from 'redux-saga'
 
 import { submitUser, loginUser } from '../services/userApi'
-import { START_LOG_IN, START_REGISTER } from '../actions/types'
+import {
+  START_LOG_IN,
+  START_REGISTER,
+} from '../actions/types'
+import { loginSucceeded, registerSucceeded } from '../actions/creators'
+import { saveToken } from '../auth'
 
-function* startUserSubmit(user) {
-	
+function createSubmitSaga({ submitFn, _error, submitSuccessCreator }) {
+	return function* (user) {
+		try {
+			const json = yield submitFn(user)
+			yield call(saveToken, json.access_token)
+			yield put(submitSuccessCreator())
+		} catch (err) {
+			throw new SubmissionError({
+				_error 
+			})
+		}
+	}
 }
 
+const startUserSubmit = createSubmitSaga({
+	submitFn: submitUser,
+	_error: 'Register failed, please try again.',
+	submitSuccessCreator: loginSucceeded,
+})
+
+const startUserLogin = createSubmitSaga({
+	submitFn: submitUser,
+	_error: 'Register failed, please try again.',
+	submitSuccessCreator: loginSucceeded,
+})
+
 export function* watchStartRegister() {
-	yield takeEvery(START_REGISTER, startUserSubmit);
+  yield takeLatest(START_REGISTER, startUserSubmit)
+}
+
+export function* watchStartLogin() {
+  yield takeLatest(START_LOG_IN, startUserLogin)
 }
