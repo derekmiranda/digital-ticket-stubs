ab075df0190d0409d4564f308a6c3baaa6a0e273
Derek Miranda <derek.miranda.1992@gmail.com>
2018-02-23 17:33:20 -0800

    login status reducers

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,18 +1,13 @@
-import {
-  combineReducers
-} from 'redux';
-import {
-  reducer as formReducer
-} from 'redux-form';
+import { combineReducers } from 'redux'
+import { reducer as formReducer } from 'redux-form'
 
-import ticketsForm from './ticketsForm';
-import submittingTickets from './submittingTickets';
-import initialViewings from './initialViewings';
-import watchtimeWarnings from './watchtimeWarnings';
-import searchResults from './searchResults';
-import {
-  reducerFromObj
-} from 'client/utils/reducerUtils';
+import ticketsForm from './ticketsForm'
+import submittingTickets from './submittingTickets'
+import initialViewings from './initialViewings'
+import watchtimeWarnings from './watchtimeWarnings'
+import searchResults from './searchResults'
+import { reducerFromObj } from 'client/utils/reducerUtils'
+import { ticketsFormName } from 'client/constants'
 import {
   START_TICKETS_LOAD,
   TICKETS_LOAD_SUCCEEDED,
@@ -20,43 +15,52 @@ import {
   FOCUS_VIEWING,
   LOADED_SEARCH_RESULTS,
   WATCHTIME_VALID,
-  CHECK_LOGIN,
-} from 'actions/types';
-import {
-  ticketsFormName
-} from 'client/constants';
+  LOG_IN_SUCCEEDED,
+  REGISTER_SUCCEEDED
+} from '../actions/types'
 
 const rootReducer = combineReducers({
   form: formReducer.plugin({
-    [ticketsFormName]: ticketsForm,
+    [ticketsFormName]: ticketsForm
   }),
   submittingTickets,
   initialViewings,
   watchtimeWarnings,
   searchResults,
-  loading: reducerFromObj({
-    [START_TICKETS_LOAD]: () => true,
-    [TICKETS_LOAD_SUCCEEDED]: () => false,
-    [TICKETS_LOAD_FAILED]: () => false,
-  }, false),
-  lastValidWatchtimes: reducerFromObj({
-    [WATCHTIME_VALID]: (state, action) => ({
-      ...state,
-      [action.formId]: action.watchtime,
-    }),
-    [TICKETS_LOAD_SUCCEEDED]: (state, action) => {
-      const initialWatchtimes = action.loadedTickets
-        .reduce((accum, ticket) => {
-          accum[ticket.formId] = ticket.watchtime;
-          return accum;
-        }, {})
-      return { ...state, ...initialWatchtimes }
-    }
-  }, {}),
-  loggedIn: reducerFromObj({
-    [CHECK_LOGIN]: ({ loggedIn }) => loggedIn,
-
-  }, false)
+  loading: reducerFromObj(
+    {
+      [START_TICKETS_LOAD]: () => true,
+      [TICKETS_LOAD_SUCCEEDED]: () => false,
+      [TICKETS_LOAD_FAILED]: () => false
+    },
+    false
+  ),
+  lastValidWatchtimes: reducerFromObj(
+    {
+      [WATCHTIME_VALID]: (state, action) => ({
+        ...state,
+        [action.formId]: action.watchtime
+      }),
+      [TICKETS_LOAD_SUCCEEDED]: (state, action) => {
+        const initialWatchtimes = action.loadedTickets.reduce(
+          (accum, ticket) => {
+            accum[ticket.formId] = ticket.watchtime
+            return accum
+          },
+          {}
+        )
+        return { ...state, ...initialWatchtimes }
+      }
+    },
+    {}
+  ),
+  loggedIn: reducerFromObj(
+    {
+      [LOG_IN_SUCCEEDED]: () => true,
+      [REGISTER_SUCCEEDED]: () => true
+    },
+    false
+  )
 })
 
-export default rootReducer;
+export default rootReducer
